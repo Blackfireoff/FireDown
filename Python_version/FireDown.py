f5ed5d0b6a520304f7b12bbc7948f1d91fc76682
leonardoheron <leonardo.heron@etu.univ-st-etienne.fr>
2023-05-19 21:20:50 +0200

    Adding Help section and a better progress bar for one shot file

--- Python_version/FireDown.py
+++ Python_version/FireDown.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import io
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets, uic
 import yt_dlp
 import json
 
@@ -19,6 +22,8 @@ class MainWindow(QtWidgets.QMainWindow):
         if data['save_path_enable'] == "True":
             self.actionSave_the_path.setChecked(True)
             self.lineEdit_path.setText(data['save_path'])
+        if data['show_advance_log'] == "True":
+            self.actionShow_advance_logs.setChecked(True)
         self.langue(data['language_select'])
         self.pushButton_download.clicked.connect(self.ok_button_clicked.emit)
         self.pushButton_download.clicked.connect(self.handle_ok_button)
@@ -29,10 +34,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.actionSave_the_path.triggered.connect(self.save_path)
         self.progressBar_items.hide()
         self.pushButton_path.clicked.connect(self.open_directory_dialog)
+        self.actionShowSupportedWebsite.triggered.connect(self.open_supported_websites)
+        self.actionShow_advance_logs.triggered.connect(self.show_advance_log)
 
+        self.cancel_button_clicked.connect(self.handle_cancel_button)
 
-
-        self.setWindowTitle("VOD_Download")
+        self.setWindowTitle("FireDown")
         icon_path = "icon.png"
         icon = QtGui.QIcon(icon_path)
         self.setWindowIcon(icon)
@@ -47,6 +54,24 @@ class MainWindow(QtWidgets.QMainWindow):
     #    dialogue.addButton(QtWidgets.QMessageBox.Ok)
     #    dialogue.exec_()
 
+    def show_advance_log(self):
+        with open('global_var.json', 'r') as f:
+            data = json.load(f)
+        if self.actionShow_advance_logs.isChecked():
+            data['show_advance_log'] = "True"
+        else:
+            data['show_advance_log'] = "False"
+        with open('global_var.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        print("[JSON] Global_var - show_advance_log : " + data['show_advance_log'])
+
+
+    def open_supported_websites(self):
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md"))
+
+    def handle_cancel_button(self):
+        self.worker.finished.emit()
+
     def save_path(self):
         with open('global_var.json', 'r') as f:
             data = json.load(f)
@@ -58,8 +83,8 @@ class MainWindow(QtWidgets.QMainWindow):
             data['save_path_enable'] = "False"
         with open('global_var.json', 'w') as f:
             json.dump(data, f, indent=4)
-        # print("[JSON] Global_var - save_path : " + data['save_path'])
-        # print("[JSON] Global_var - save_path_enable : " + data['save_path_enable'])
+        print("[JSON] Global_var - save_path : " + data['save_path'])
+        print("[JSON] Global_var - save_path_enable : " + data['save_path_enable'])
 
     def selec_fr(self):
         self.langue("FR")
@@ -85,6 +110,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pushButton_cancel.setText(data[langue]['ButtonCancel'])
         self.menuSettings.setTitle(data[langue]['SettingMenu'])
         self.menuLanguage.setTitle(data[langue]['LanguageMenu'])
+        self.menuHelp.setTitle(data[langue]['HelpMenuTitle'])
+        self.actionShowSupportedWebsite.setText(data[langue]['ShowSupportedWebsite'])
+        self.actionShow_advance_logs.setText(data[langue]['ShowAdvanceLog'])
 
         with open('global_var.json', 'r') as f:
             data = json.load(f)
@@ -92,7 +120,8 @@ class MainWindow(QtWidgets.QMainWindow):
         with open('global_var.json', 'w') as f:
             json.dump(data, f, indent=4)
 
-
+    def show_avancement_in_log(self,param):
+        self.plainTextEdit_output.appendHtml(f"<span style='color: black;'>{str(param)}</span>")
 
     def append_html_to_plain_text_edit(self):
         self.plainTextEdit_output.appendHtml("<span style='color: orange;'>Téléchargement(s) en cours... </span><br>")
@@ -101,15 +130,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.plainTextEdit_output.appendHtml("<span style='color: green;'>Téléchargement(s) réussit !</span><br>")
 
     def current_index(self, param):
-        # print("[Info] - Nbr current :" + str(param))
+        print("[Info] - Nbr current :" + str(param))
         self.progressBar_items.setValue(param)
 
     def init_index(self, param):
-        # print("[Info] - Nbr max :" + str(param))
+        print("[Info] - Nbr max :" + str(param))
         self.progressBar_items.setMaximum(param)
 
     def show_error(self, param):
-        # print(param)
+        print(param)
         self.plainTextEdit_output.appendHtml("<span style='color: red;'>/!\ Erreur rencontrée /!\</span><br>")
 
     def handle_ok_button(self):
@@ -119,23 +148,27 @@ class MainWindow(QtWidgets.QMainWindow):
         current_index = self.comboBox_format.currentIndex()
         self.append_html_to_plain_text_edit()
         self.save_path()
-        # print("url : " + url)
-        # print("path : " + path)
-        # print("current_index : " + str(current_index))
+        print("url : " + url)
+        print("path : " + path)
+        print("current_index : " + str(current_index))
 
         self.progressBar_items.show()
+
+        self.pushButton_cancel.clicked.connect(self.cancel_button_clicked.emit)
 
         # Step 2: Create a QThread object
         self.thread = QtCore.QThread()
         # Step 3: Create a worker object
         self.worker = Worker(url, path, current_index)
         # Step 4: Move worker to the thread
+
         self.worker.moveToThread(self.thread)
         # Step 5: Connect signals and slots
         self.thread.started.connect(self.worker.run)
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.worker.progress.connect(self.current_index)
+        self.worker.percentage_progress.connect(self.show_avancement_in_log)
         self.worker.init_val.connect(self.init_index)
         self.worker.error.connect(self.show_error)
 
@@ -169,7 +202,7 @@ class MainWindow(QtWidgets.QMainWindow):
         if directory_dialog.exec_():
             selected_directory = directory_dialog.selectedFiles()
             if selected_directory:
-                # print("Chemin du dossier sélectionné :", selected_directory[0])
+                print("Chemin du dossier sélectionné :", selected_directory[0])
                 self.lineEdit_path.setText(selected_directory[0])
                 self.save_path()
 
@@ -178,6 +211,7 @@ class Worker(QtCore.QObject):
     finished = QtCore.pyqtSignal()
     progress = QtCore.pyqtSignal(int)
     init_val = QtCore.pyqtSignal(int)
+    percentage_progress = QtCore.pyqtSignal(str)
     error = QtCore.pyqtSignal(str)
 
     def __init__(self, url, path, index):
@@ -186,6 +220,7 @@ class Worker(QtCore.QObject):
         self.path = path
         self.index = index
         self.yt = yt_dlp
+
 
     def run(self):
         if self.index == 0:
@@ -199,14 +234,17 @@ class Worker(QtCore.QObject):
             self.finished.emit()
 
     def postprocessor_hook(self, info):
-        # print(info)
+        with open('global_var.json', 'r') as f:
+            data = json.load(f)
+        if data['show_advance_log'] == "True":
+            self.percentage_progress.emit(info['_default_template'])
+        if '__last_playlist_index' not in info['info_dict']:
+            self.progress.emit(int(info['_percent_str'].split(".")[0]))
+            self.init_val.emit(100)
         if info['status'] == 'finished':
             if '__last_playlist_index' in info['info_dict']:
                 self.progress.emit(info['info_dict']['playlist_index'])
                 self.init_val.emit(info['info_dict']['__last_playlist_index'])
-            else:
-                self.progress.emit(1)
-                self.init_val.emit(1)
         if info['status'] == 'error':
             # print(info)
             self.error.emit(info)
@@ -239,7 +277,7 @@ class Worker(QtCore.QObject):
                 'preferredcodec': 'mp3',
             }],
             'progress_hooks': [self.postprocessor_hook],
-            'quiet': True
+            'quiet': 1
         }
         try:
             self.yt.YoutubeDL(options).download(self.url)
@@ -260,6 +298,11 @@ class Worker(QtCore.QObject):
 
 
 if __name__ == '__main__':
+    if sys.stderr is None:
+        stream = io.StringIO()
+        sys.stdout = stream
+        sys.stderr = stream
+
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
 
