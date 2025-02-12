b6d17d42d1f82def80d7cbb87e34c5f010b4ac12
Leoozzz <lheron@firestudio.tech>
2025-02-12 07:31:04 +0100

    re;oving cookies

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -174,15 +174,6 @@ async def download_video(url: str, format_type: str, quality: str, file_format: 
             'rm_cachedir': True,
             'ffmpeg_location': ffmpeg_location,
             'retries': 10,
-            'socket_timeout': 30,
-            'cookiesfrombrowser': ('chrome',),  # Utiliser les cookies de Chrome
-            'http_headers': {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language': 'en-us,en;q=0.5',
-                'Sec-Fetch-Mode': 'navigate',
-            },
-            'cachedir': False,
         }
 
         if format_type == "audio":
